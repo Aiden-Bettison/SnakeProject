27d19ab09a71619930d0818ca2745803fa78c5b7
Aiden Bettison <aiden.bettison@student.allenisd.org>
2022-05-09 09:55:27 -0500

    MOVEMENTS WORKING BOYS

--- Sources/ScenesShell/InteractionLayer.swift
+++ Sources/ScenesShell/InteractionLayer.swift
@@ -13,7 +13,16 @@ class InteractionLayer : Layer, KeyDownHandler {
     var snakes = [Snake(rect: Rect(size: Size(width: 25, height:25)))]
     var n = 1
     var head = 0
+    var tails = [0]
+    var tail = 0
+    var size = 0
     var isFacing = ""
+   
+   
+
+
+
+    
 
     func gridArray() {
         snakes[0].move(to: Point(x: 324, y: 102))
@@ -33,13 +42,21 @@ class InteractionLayer : Layer, KeyDownHandler {
               
                   n += 1
               }
-          }
+        }
+        tails.remove(at: 0)
+        
           for grid in 0 ..< n {
-              snakes[grid].rectangle.fillMode = .stroke  
+              snakes[grid].fillStyle = FillStyle(color: Color(.white))  
           }
           for snake in 717 ..< 720 {
-              head = 720
-              snakes[snake].rectangle.fillMode = .fillAndStroke
+              head = 719
+              if snake != head {
+                  tails.insert(snake, at: 0)
+                  size += 1
+              }
+              print(tails)
+              snakes[snake].fillStyle = FillStyle(color: Color(.green))
+              
           }
     }
 
@@ -52,21 +69,27 @@ class InteractionLayer : Layer, KeyDownHandler {
            if isFacing != "down" {
                isFacing = "up"
            }
+
+           
        case "w":
            if isFacing != "down" {
                isFacing = "up"
            }
+
+
 
        case "ArrowLeft":
            if isFacing != "right" {
                isFacing = "left"
            }
 
+
        case "a":
            if isFacing != "right" {
                isFacing = "left"
            }
            
+
        case "ArrowDown":
            if isFacing != "up" {
                isFacing = "down"
@@ -82,19 +105,62 @@ class InteractionLayer : Layer, KeyDownHandler {
                isFacing = "right"
            }
 
+
        case "d":
            if isFacing != "left" {
                isFacing = "right"
            }
 
+          
 
+           
        default:
            print("Fatal Error: not a direction key.")
        }
 
-       print(isFacing)
-            
-      
+       // print(isFacing)
+
+       switch isFacing {
+       case "up":
+               tail = tails.last!
+               //snakes[head - 50].rectangle.fillMode = .fillAndStroke
+               snakes[head - 50].fillStyle = FillStyle(color: Color(.green))
+               snakes[tail].fillStyle = FillStyle(color: Color(.white))
+               tails.insert((head), at: 0)
+               tails.remove(at: size)
+               head -= 50
+          
+       case "down":
+               tail = tails.last!
+               //snakes[head - 50].rectangle.fillMode = .fillAndStroke
+               snakes[head + 50].fillStyle = FillStyle(color: Color(.green))
+               snakes[tail].fillStyle = FillStyle(color: Color(.white))
+               tails.insert((head), at: 0)
+               tails.remove(at: size)
+               head += 50
+          
+       case "left":          
+                tail = tails.last!
+               snakes[head - 1].fillStyle = FillStyle(color: Color(.green))
+               snakes[tail].fillStyle = FillStyle(color: Color(.white))
+               tails.insert((head), at: 0)
+               tails.remove(at: size)
+               head -= 1
+       case "right":
+                tail = tails.last!
+       //        snakes[head + 1].rectangle.fillMode = .fillAndStroke
+             snakes[head + 1].fillStyle = FillStyle(color: Color(.green))
+             snakes[tail].fillStyle = FillStyle(color: Color(.white))
+               tails.insert(head, at: 0)
+               tails.remove(at: size)
+               head += 1
+       default:
+           print("Snake is not facing a direction")
+           }
+           
+
+           
+
     }
 
 
--- Sources/ScenesShell/Snake.swift
+++ Sources/ScenesShell/Snake.swift
@@ -2,98 +2,27 @@ import Foundation
 import Scenes
 import Igis
 
-/* struct Snake {
 
- //   var velocityX = 0
- //   var velocityY = 0
-    
-   
-
-                   
- 
-            
-        init(head: Coordinate) {
-        self.init(head, [], .north)
-        /*rectangle = Rectangle(rect:rect, fillMode:.fillAndStroke)
-        GameOverText = Text(location: Point(x: 0, y: 0), text: "GAME OVER")
-        GameOverText.font = "60pt Arial"
-        
-         */
-
-        // Using a meaningful name can be helpful for debugging
-//        super.init(name: "Snake")
-    }
-
-        enum Facing { case north, east, south, west }
-        enum Move {case forward, right, left }
-        enum Change { case move(Move), grow  }
-
-        let head  : Coordinate
-
-        let tail  : [Coordinate]
-        
-        var body  : [Coordinate] { [head] + tail }
-
-        let facing : Facing
-
-
-        func alter(_ change: Change) -> Self {
-            func growBody() -> [Coordinate] {
-                tail.last != nil ? tail + [tail.last!] : [head + tail]
-            }
-
-            switch change {
-            case let .move(direction):
-                return move (direction)
-
-            case .grow:
-                return .init(head, growBody(), facing)
-
-            }
-        }
-
-        private init(_h: Coordinate, _ t: [Coordinate], _ f : Facing) {
-            head = h; tail = t; facing = f
-        }
-        private func move(_ m: Move) -> Self {
-            func newSnakeBody() -> [Coordinate] {
-                Array(([head] + tail).prefix(max(tail.count, 0)))
-            }
-
-            switch (m, facing) {
-            case (.forward, .north): return .init((x: head.x,   y: head.y - 1), newSnakeBody(), .north)
-            case (.forward, .east ): return .init((x:head.x + 1, y:head.y    ), newSnakeBody(), .east )
-            case (.forward, .south): return .init((x:head.x,     y:head.y + 1), newSnakeBody(), .south)
-            case (.forward, .west ): return .init((x:head.x - 1, y:head.y    ), newSnakeBody(), .west )
-            case (   .left, .north): return .init((x:head.x - 1, y:head.y    ), newSnakeBody(), .west )
-            case (   .left, .east ): return .init((x:head.x,     y:head.y - 1), newSnakeBody(), .north)
-            case (   .left, .south): return .init((x:head.x + 1, y:head.y    ), newSnakeBody(), .east )
-            case (   .left, .west ): return .init((x:head.x,     y:head.y + 1), newSnakeBody(), .south)
-            case (  .right, .north): return .init((x:head.x + 1, y:head.y    ), newSnakeBody(), .east )
-            case (  .right, .east ): return .init((x:head.x,     y:head.y + 1), newSnakeBody(), .south)
-            case (  .right, .south): return .init((x:head.x - 1, y:head.y    ), newSnakeBody(), .west )
-            case (  .right, .west ): return .init((x:head.x,     y:head.y - 1), newSnakeBody(), .north)
-            }
-        }
-
-        
- */
         class Snake : RenderableEntity{
     let GameOverText : Text
     var rectangle: Rectangle
     var GameOver = false
 
 
-    let  strokeStyle = StrokeStyle(color:Color(.black))
-        let fillStyle = FillStyle(color:Color(.green))
-        let lineWidth = LineWidth(width:2)
+    var strokeStyle : StrokeStyle
+    var fillStyle : FillStyle
+    var lineWidth = LineWidth(width:2)
+ 
 
         
-            init(rect: Rect) {
+    init(rect: Rect) {
  //           let rect = Rect(topLeft: head, size: Size(width: 25, height: 25))
                 rectangle = Rectangle(rect: rect, fillMode: .fillAndStroke)
             GameOverText = Text(location: Point(x: 0, y: 0), text: "GAME OVER")
-            GameOverText.font = "60pt Arial"            
+            GameOverText.font = "60pt Arial"
+            strokeStyle = StrokeStyle(color: Color(.black))
+            fillStyle = FillStyle(color: Color(.green))
+
             }
 
             
@@ -111,7 +40,7 @@ import Igis
 
     override func calculate(canvasSize: Size) {
         
-        GameOverText.location = Point(x: canvasSize.center.x - 200, y: canvasSize.center.y)
+/*        GameOverText.location = Point(x: canvasSize.center.x - 200, y: canvasSize.center.y)
   //      move(velocityX: velocityX, velocityY: velocityY)
 
          
@@ -135,14 +64,19 @@ import Igis
 
         if tooFarUp || tooFarDown {
             GameOver = true
-        }
+            }
+            
+ */
         }
     
 
+    
     override func render(canvas:Canvas) {
-if GameOver == true {
+/*if GameOver == true {
      canvas.render(GameOverText)
-}
+     }
+     
+ */
 canvas.render(strokeStyle, fillStyle,  lineWidth, rectangle)
     }        
         }
